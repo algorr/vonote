6c013763a14ff209c0cfb487fbc54fd0145e4465
Ümit Kar <umitkar08@gmail.com>
2022-10-03 14:09:20 +0300

    Update splash_screen
    updated splash_screen

--- lib/core/home/view/splash_screen_view.dart
+++ lib/core/home/view/splash_screen_view.dart
@@ -5,7 +5,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vonote/app/routes/routes.dart';
 import 'package:vonote/app/viewmodel/app/app_bloc.dart';
 
-
 class SplashScreenView extends StatelessWidget {
   const SplashScreenView({Key? key}) : super(key: key);
 
@@ -17,38 +16,56 @@ class SplashScreenView extends StatelessWidget {
       builder: (context, state) {
         return MaterialApp(
           theme: ThemeData(primarySwatch: Colors.red),
-          home: Scaffold(
-            body: EasySplashScreen(
-              logo: Image.asset(
-                'assets/images/logo.png',
-                width: 200,
-                height: 200,
-              ),
-              loaderColor: Colors.red.shade800,
-              logoWidth: 250,
-              title: const Text(
-                "Simplier Way of Splash Screen",
-                style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black),
-              ),
-              backgroundColor: Colors.black38,
-              showLoader: false,
-              loadingText: const Text("Please Wait..."),
-              navigator: FlowBuilder(
-                state: context.select(
-                  (AppBloc bloc) => bloc.state.status,
-                ),
-                onGeneratePages: (AppStatus state, List<Page<dynamic>> pages) {
-                 return  OnGenerateViews(state, pages);
-                },
-              ),
-              durationInSeconds: 5,
-            ),
+          home: const Scaffold(
+            body: CustomSplashScreenAndFlowBuilder(),
           ),
         );
       },
     );
   }
+}
+
+class CustomSplashScreenAndFlowBuilder extends StatelessWidget {
+  const CustomSplashScreenAndFlowBuilder({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return EasySplashScreen(
+      logo: Image.asset(
+        'assets/images/logo.png',
+        width: SplashScreenConsts.imageWidth,
+        height: SplashScreenConsts.imageHeight,
+      ),
+      loaderColor: Colors.red.shade800,
+      logoWidth: SplashScreenConsts.logoWidth,
+      title: const Text(
+        SplashScreenConsts.title,
+        style: SplashScreenConsts.titleStyle,
+      ),
+      backgroundColor: Colors.black38,
+      showLoader: false,
+      loadingText: const Text(SplashScreenConsts.loadingTitle),
+      navigator: FlowBuilder(
+        state: context.select(
+          (AppBloc bloc) => bloc.state.status,
+        ),
+        onGeneratePages: (AppStatus state, List<Page<dynamic>> pages) {
+          return OnGenerateViews(state, pages);
+        },
+      ),
+      durationInSeconds: 5,
+    );
+  }
+}
+
+class SplashScreenConsts {
+  static const String title = 'The Simplier Way of Splash Screen';
+  static const String loadingTitle = 'Please Wait..';
+  static const TextStyle titleStyle =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black);
+  static const double logoWidth = 250;
+  static const double imageWidth = 200;
+  static const double imageHeight = 200;
 }
